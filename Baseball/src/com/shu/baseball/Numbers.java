b4c43a630b80faaecdb65cd4facf3665405087f7
Sooin <sooinc91@naver.com>
2022-07-31 19:19:40 +0900

    7/31 save

--- Baseball/src/com/shu/baseball/Numbers.java
+++ Baseball/src/com/shu/baseball/Numbers.java
@@ -2,54 +2,67 @@ package com.shu.baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Numbers {
 	private final List<Number> numbers;
-	
-	int strikecount=0;
-	int ballcount=0;
-	int outcount=0;
-	int trycount=0;
-	
+	int strikecount = 0;
+	int ballcount = 0;
+	int outcount = 0;
+	int trycount = 0;
+
 	public Numbers(String numbers) {
 		this.numbers = mapNumbers(numbers);
-		
+
 	}
 
 	private List<Number> mapNumbers(String numbers) {
 		List<Number> temp = new ArrayList<>();
-		
+
 		String[] numberArray = numbers.split("");
-		for (int position=0; position<numberArray.length; position++) {
+		for (int position = 0; position < numberArray.length; position++) {
 			int value = Integer.parseInt(numberArray[position]);
 			temp.add(new Number(position, value));
 		}
-		
+
 		return temp;
 	}
-	
-	public void makeComnumber() {
-		//정답숫자 생성
+
+	public List<Number> makeComnumber() {
+		List<Number> comNums = new ArrayList<>();// 정답숫자 생성
+		Random random = new Random();
+		for (int position = 0; position < 3; position++) {
+			int value = random.nextInt(10);
+			comNums.add(new Number(position, value));
+		}
+		return comNums;
 	}
-	
-	public void strikeCount(){
+
+	public void strikeCount() {
 		this.strikecount++;
 	}
-	public void ballCount(){
+
+	public void ballCount() {
 		this.ballcount++;
 	}
-	public void outCount(){
+
+	public void outCount() {
 		this.outcount++;
 	}
-	public void tryCount(){
+
+	public void tryCount() {
 		this.trycount++;
 	}
 
-	public String compare(Numbers plyNums1) {
-//		for() {
-			
-			
-//		}
+	public String compare(Numbers comNums, Numbers plyNums) {
+		String result;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				result = "";
+				System.out.println(result);
+			}
+
+		}
 		return null;
 	}
 }
